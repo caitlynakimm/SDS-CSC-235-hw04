3fe68269e59c7d68dbb31f8420c88146cac754da
Caitlyn Kim <cakim@smith.edu>
2026-03-27 02:29:53 -0400

    working on creating graph in forced-directed layout and decluttering it

--- index.js
+++ index.js
@@ -2,3 +2,84 @@ d3.json("adj_noun.json")
     .then(function(data) {
         console.log(data); 
     }); 
+
+d3.json("adj_noun.json").then(function (data) {
+    const width = 700;
+    const height = 700;
+
+    const color = d3.scaleOrdinal(d3.schemeTableau10);
+
+    const svg = d3.select("#frame")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("border", "1px solid #000");
+
+    const simulation = d3.forceSimulation(data.nodes)
+        .force("link", d3.forceLink(data.links).id(d => d.id).distance(80)) //connects edges to nodes, set edge lengths to be 60 pixels
+        .force("charge", d3.forceManyBody().strength(-200)) //repels nodes by strength of 100
+        .force("center", d3.forceCenter(width/2, height/2)) //positions graph into center of svg
+        .force("collision", d3.forceCollide(10)); //makes nodes bump into each other
+
+    const link = svg.append("g")
+        .attr("stroke", "#878181ff")
+        .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(data.links)
+        .join("line");
+
+    const node = svg.append("g")
+        .selectAll("circle")
+        .data(data.nodes)
+        .join("circle")
+        .attr("r", 8)
+        .attr("fill", d => color(d.type))
+        .attr("stroke", "#000")
+        .call(drag(simulation)); //makes nodes draggable
+
+    const label = svg.append("g")
+        .selectAll("text")
+        .data(data.nodes)
+        .join("text")
+        .text(d => d.id)
+        .attr("font-size", 10)
+        .attr("dx", -8)
+        .attr("dy", -10) //dx and dy - positions label relative to node's center
+        .attr("fill", "#000")
+        .style("pointer-events", "none"); //interactions on labels are ignored
+
+    simulation.on("tick", () => { //updates positions of links, nodes, and labels for every frame
+        link
+            .attr("x1", d => d.source.x) //line's starting x
+            .attr("y1", d => d.source.y) //line's starting y
+            .attr("x2", d => d.target.x) //line's ending x
+            .attr("y2", d => d.target.y); //line's ending y
+        
+        node
+            .attr("cx", d => d.x) //node's center x
+            .attr("cy", d => d.y); //node's center y
+
+        label
+            .attr("x", d => d.x)
+            .attr("y", d => d.y);
+    });
+
+    function drag(simulation) {
+        return d3.drag()
+            .on("start", (event, d) => {
+                if (!event.active) simulation.alphaTarget(0.3).restart(); //controlling simulation's node movement energy
+                d.fx = d.x;
+                d.fy = d.y; //mouse starts drag, holding onto node, node stays at current position
+            })
+            .on("drag", (event, d) => {
+                d.fx = event.x;
+                d.fy = event.y; //node follows mouse's changing position
+            })
+            .on("end", (event, d) => {
+                if (!event.active) simulation.alphaTarget(0);
+                d.fx = null;
+                d.fy = null; //node no longer follows mouse's position
+            });
+    }
+
+});
